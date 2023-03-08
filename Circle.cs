3977b099254a01ac51586c0ccfdf9d591c04f40a
Nigel <m0cvo@yahoo.co.uk>
2023-03-08 13:01:55 +0000

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    internal class Circle
+    {
+        /// <summary>
+        /// Standard mathematical formulae for calculating area and circumference of a circle object.
+        /// </summary>
+        /// <param name="rad"></param>
+        /// <returns></returns>
+
+        public static float Circumference(string rad)
+        {
+            double R = Double.Parse(rad);
+            float c = (float)(R * R);
+            return c;
+        }
+
+        public static float Area(string rad)
+        {
+            double R = Double.Parse(rad);
+            float A = (float)(Math.PI * R);
+            return A;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using static System.Console;
+
+namespace Circle
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        { 
+            WriteLine("Circle calculator by N.Booth 2023 for Mogpie \n");
+            WriteLine("(1) Circumference of circle ");
+            WriteLine("(2) Area of circle ");
+            Write(": ");
+
+            string choice = ReadLine();
+            double A, C = 0;
+
+            switch (choice) 
+            {
+                case "1":
+                    Write("Please enter the radius in mm: ");
+                    C = Circle.Circumference(ReadLine() ?? "0");
+                    WriteLine("Circumference of circle is: {0:F2}mm", C);
+                    break;
+
+                case "2":
+                    Write("Please enter the radius in mm: ");
+                    A = Circle.Area(ReadLine() ?? "0");
+                    WriteLine("Area of the circle is: {0:F2}mm", A);
+                    break;
+
+                default:
+                    WriteLine("Please enter a choice.");
+                    break;
+            }
+
+            // Keep the console window open whilst in debug mode.
+            WriteLine("Press any key to exit.");
+            ReadKey();
+        } 
+    }
+}
